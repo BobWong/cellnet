c4548aa63f292a5562ea5d68669ab46daedbd4e1
BobWong <bin.wang@igg.com>
2018-08-07 17:07:46 +0800

    1.增加sproto支持

---
+++ codec/sproto/sproto.go
@@ -0,0 +1,71 @@
+package sprotocodec
+
+import (
+	"reflect"
+
+	"github.com/davyxu/cellnet"
+	"github.com/davyxu/gosproto"
+	"fmt"
+	"path"
+	"github.com/davyxu/cellnet/util"
+	"github.com/davyxu/cellnet/codec"
+)
+
+type sprotoCodec struct {
+}
+
+func (self *sprotoCodec) Name() string {
+	return "sproto"
+}
+
+func (self *sprotoCodec) MimeType() string {
+	return "application/sproto"
+}
+
+func (self *sprotoCodec) Encode(msgObj interface{},ctx cellnet.ContextSet) (data interface{}, err error) {
+
+	result, err := sproto.Encode(msgObj)
+	if err != nil {
+		return nil, err
+	}
+
+	return sproto.Pack(result), nil
+}
+
+func (self *sprotoCodec) Decode(data interface{}, msgObj interface{}) error {
+	tmp := data.([]byte)
+	// sproto要求必须有头, 但空包也是可以的
+	if len(tmp) == 0 {
+		return nil
+	}
+
+	raw, err := sproto.Unpack(tmp)
+	if err != nil {
+		return err
+	}
+
+	_, err2 := sproto.Decode(raw, msgObj)
+
+	return err2
+}
+
+func AutoRegisterMessageMeta(msgTypes []reflect.Type) {
+
+	for _, tp := range msgTypes {
+
+		msgName := fmt.Sprintf("%s.%s", path.Base(tp.PkgPath()), tp.Name())
+
+		//cellnet.RegisterMessageMeta("sproto", msgName, tp, util.StringHash(msgName))
+		cellnet.RegisterMessageMeta(&cellnet.MessageMeta{
+			Codec:codec.MustGetCodec("sproto"),
+			Type: tp,
+			ID: int(util.StringHash(msgName)),
+		})
+	}
+
+}
+
+func init() {
+
+	codec.RegisterCodec(new(sprotoCodec))
+}
